f3f06e40ac6fde6b2d4b8e74889e90870569fc60
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-08-19 11:00:26 -0300

    OrderViewSet com TokenAuth + testes autenticados

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -1,10 +1,12 @@
 
 
 
+
 import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 
 from django.urls import reverse
 
@@ -19,6 +21,12 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory()
+        token = Token.objects.create(user=self.user)
+        token.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title='mouse', price=100, category=[self.category])
         self.order = OrderFactory(product=[self.product])
@@ -30,11 +38,11 @@ class TestOrderViewSet(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        order_data= json.loads(response.content)[0]
-        self.assertEqual(order_data['product'][0]['title'], self.product.title)
-        self.assertEqual(order_data['product'][0]['price'], self.product.price)
-        self.assertEqual(order_data['product'][0]['active'], self.product.active)
-        self.assertEqual(order_data['product'][0]['category'][0]['title'], self.category.title)
+        order_data= json.loads(response.content)
+        self.assertEqual(order_data['results'][0]['product'][0]['title'], self.product.title)
+        self.assertEqual(order_data['results'][0]['product'][0]['price'], self.product.price)
+        self.assertEqual(order_data['results'][0]['product'][0]['active'], self.product.active)
+        self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)
 
     def test_create_order(self):
         user = UserFactory()
--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -3,12 +3,17 @@
 
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.all().order_by('id')
