affc35c3f8a6ea4076286466b987751bd60b7602
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-09-08 10:28:26 -0300

    fix(routes): redirect / -> admin usando RedirectView

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,30 +1,19 @@
-"""
-URL configuration for bookstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.generic import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
 
 urlpatterns = [
-    path('__debug__/', include(debug_toolbar.urls)),
-    path("", lambda r: redirect("/admin/")),
+    path("", RedirectView.as_view(pattern_name="admin:index", permanent=False)),
     path("admin/", admin.site.urls),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path(r"^bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
